cf6163bb5095aa12b7cb69442254a91cf46be35a
nathanieltan <nathaniel98.tan@gmail.com>
2017-03-31 22:57:32 -0400

    Created a main game class and an instance of said class. Rearranged code to fit within new structure

--- Game.py
+++ Game.py
@@ -1,8 +1,16 @@
 import pygame
 from PIL import Image
 import os, sys
+clock = pygame.time.Clock()
+im = Image.open('TeddyBear.jpg')
+im.thumbnail((64, 64), Image.ANTIALIAS)
+im.save('ThisGuy.jpg', "JPEG")
+assemblerpng = Image.open('Assembler.png')
 
-class Producer():
+assemblerimg = pygame.image.load('Assembler.png')
+bearimg = pygame.image.load('ThisGuy.jpg')
+
+class Producer(pygame.sprite.Sprite):
     def __init__(self, product, x=-1, y=-1, progress=0, production=100, built=False):
         self.product = product
         self.x = x
@@ -11,19 +19,9 @@ class Producer():
         self.production = production
         self.built = built
         self.t = 0
-        self.p = 0
 
-    def render(self, screen, assemblerimg, assemblerpng, bearimg):
-        screen.blit(assemblerimg, (self.x, self.y))
-        if self.built:
-            if self.t < 30:
-                self.t += 1
-                screen.blit(bearimg, (self.x + (assemblerpng.size[0]/2), ((self.y/2)-2*self.t) + (assemblerpng.size[1]/2)))
-            else:
-                self.t = 0
-                self.built = False
 
-    def step(self, button):
+    def step(self, button, screen):
         self.progress += 3
         pygame.draw.rect(screen, (255, 255, 255), (self.progress*4, 500, 32, 100), 0)
         pygame.draw.rect(screen, (255, 255, 255), (800-self.progress*4, 500, 32, 100), 0)
@@ -34,40 +32,56 @@ class Producer():
             if button == False:
                 self.progress = 0
 
+class GameMain():
+    """Main Game Class"""
+    def __init__(self,width=800,height=600):
+        pygame.init()
+        pygame.display.set_caption('Manual Control')
+        # initializes screen
+        self.screen = pygame.display.set_mode([width, height])
+
+
+    def MainLoop(self):
+        self.LoadSprites()
+
+        running = True
+        while(running):
+            button = False
+            clock.tick(60)
+            pygame.display.update()
+            self.screen.fill((160, 82, 45))
+            event = pygame.event.poll()
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                pressed = pygame.key.get_pressed()
+                if pressed[pygame.K_0]:
+                    button = True
+            for factory in self.factories:
+                self.render(self.screen, assemblerimg, assemblerpng, bearimg)
+                factory.step(button,self.screen)
+
+    def render(self,screen,assemberimg,assemblerpng,bearimg):
+        screen.blit(assemblerimg, (self.factories[0].x, self.factories[0].y))
+        if self.factories[0].built:
+            if self.factories[0].t < 30:
+                self.factories[0].t += 1
+                screen.blit(bearimg, (self.factories[0].x + (assemblerpng.size[0]/2),
+                                      ((self.factories[0].y/2)-2*self.factories[0].t) + (assemblerpng.size[1]/2)))
+            else:
+                self.factories[0].t = 0
+                self.factories[0].built = False
+
+
+    def LoadSprites(self):
+        self.factories = []
+        for i in range(0, 1):
+            producer = Producer('teddybear', 100, 100)
+            self.factories.append(producer)
+
+
 
 
 if __name__ == '__main__':
-    factories = []
-    for i in range(0, 1):
-        producer = Producer('teddybear', 100, 100)
-        factories.append(producer)
-
-    pygame.init()
-    pygame.display.set_caption('Manual Control')
-    screen = pygame.display.set_mode([800, 600])
-    clock = pygame.time.Clock()
-
-    im = Image.open('TeddyBear.jpg')
-    im.thumbnail((64, 64), Image.ANTIALIAS)
-    im.save('ThisGuy.jpg', "JPEG")
-    assemblerpng = Image.open('Assembler.png')
-
-    assemblerimg = pygame.image.load('Assembler.png')
-    bearimg = pygame.image.load('ThisGuy.jpg')
-
-    running = True
-    while(running):
-        button = False
-        clock.tick(60)
-        pygame.display.update()
-        screen.fill((160, 82, 45))
-        event = pygame.event.poll()
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_0]:
-                button = True
-        for f in factories:
-            f.render(screen, assemblerimg, assemblerpng, bearimg)
-            f.step(button)
+    MainWindow = GameMain()
+    MainWindow.MainLoop()
