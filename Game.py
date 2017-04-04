c99ca783b14565245105efb4e489237107a01d33
Nate Sampo <nathan.sampo@students.olin.edu>
2017-04-04 12:03:48 -0400

    Zoom Zoom

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import pygame
 from PIL import Image
 import os, sys
 from Producer import Producer
+from Game_Constants import *
 clock = pygame.time.Clock()
 im = Image.open('TeddyBear.jpg')
 im.thumbnail((64, 64), Image.ANTIALIAS)
@@ -19,15 +20,17 @@ class GameMain():
         # initializes screen
         self.screen = pygame.display.set_mode([width, height])
         self.x_view = 200
-        self.y_view = 200
+        self.y_view = self.x_view * 3/4
 
 
     def MainLoop(self):
         self.LoadSprites()
-        self.scale = pygame.display.get_surface().get_size()[0]/4 # screen is 4 x 3 units initially
         self.level = 1
         running = True
         while(running):
+            self.scale = 40000/self.x_view
+            self.x_view += 0.25
+            self.y_view += 3/16
             button = False
             clock.tick(120)
             pygame.display.update()
@@ -42,30 +45,30 @@ class GameMain():
             for factory in self.factories:
                 self.render(self.screen, assemblerimg, assemblerpng, bearimg, factory)
                 factory.step(button,self.screen)
-            if self.factories[0].score >= 10:
-                self.changeLevel()
+            '''if self.factories[0].score >= 10:
+                self.changeLevel()'''
 
 
     def render(self,screen,assemberimg,assemblerpng,bearimg, factory):
         # Render inputs to factory
         progress = 1.0*factory.progress/factory.production
         if factory.inputs[0]: # up
-            pygame.draw.rect(screen, (255, 255, 255), (self.scale*(factory.x-.125), self.scale*(factory.y-(factory.inputs[0])*(1-progress)-.125), self.scale/4, self.scale/4), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (WINDOW_WIDTH/2 + self.scale*(factory.x-.125), WINDOW_HEIGHT/2 + self.scale*(factory.y-(factory.inputs[0])*(1-progress)-.125), self.scale/4, self.scale/4), 0)
         if factory.inputs[1]: # right
-            pygame.draw.rect(screen, (255, 255, 255), (self.scale*(factory.x+(factory.inputs[1])*(1-progress)-.125), self.scale*(factory.y-.125), self.scale/4, self.scale/4), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (WINDOW_WIDTH/2 + self.scale*(factory.x+(factory.inputs[1])*(1-progress)-.125), WINDOW_HEIGHT/2 + self.scale*(factory.y-.125), self.scale/4, self.scale/4), 0)
         if factory.inputs[2]: # down
-            pygame.draw.rect(screen, (255, 255, 255), (self.scale*(factory.x-.125), self.scale*(factory.y+(factory.inputs[2])*(1-progress)-.125), self.scale/4, self.scale/4), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (WINDOW_WIDTH/2 + self.scale*(factory.x-.125), WINDOW_HEIGHT/2 + self.scale*(factory.y+(factory.inputs[2])*(1-progress)-.125), self.scale/4, self.scale/4), 0)
         if factory.inputs[3]: # left
-            pygame.draw.rect(screen, (255, 255, 255), (self.scale*(factory.x-(factory.inputs[3])*(1-progress)-.125), self.scale*(factory.y-.125), self.scale/4, self.scale/4), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (WINDOW_WIDTH/2 + self.scale*(factory.x-(factory.inputs[3])*(1-progress)-.125), WINDOW_HEIGHT/2 + self.scale*(factory.y-.125), self.scale/4, self.scale/4), 0)
         # Render factory
         img = pygame.transform.scale(assemblerimg, (int(self.scale), int(self.scale)))
-        screen.blit(img, (self.scale*(factory.x-.5), self.scale*(factory.y-.5)))
+        screen.blit(img, (WINDOW_WIDTH/2 + self.scale*(factory.x-.5), WINDOW_HEIGHT/2 +self.scale*(factory.y-.5)))
         # Render output of factory
         if factory.built:
             if factory.t < 20:
                 factory.t += 0.5
                 img = pygame.transform.scale(bearimg, (int(self.scale/4), int(self.scale/4)))
-                screen.blit(img, (self.scale*(factory.x - .125),self.scale*(factory.y - .125 - factory.t/100.0)))
+                screen.blit(img, (WINDOW_WIDTH/2 + self.scale*(factory.x - .125), WINDOW_HEIGHT/2 + self.scale*(factory.y - .125 - factory.t/100.0)))
             else:
                 factory.t = 0
                 factory.built = False
@@ -75,12 +78,12 @@ class GameMain():
         self.level += 1
         self.factories[0].score = 0
         if self.level == 2:
-            self.scale *= 2.0/3
+            #self.scale *= 2.0/3
             del self.factories[:]
             self.factories.append(Producer('teddybear', 2, 2.25, [0,2,2,0]))#left
             self.factories.append(Producer('teddybear', 4, 2.25, [0,0,2,0]))#right
         elif self.level == 3:
-            self.scale *= 3.0/4
+            #self.scale *= 3.0/4
             del self.factories[:]
             self.factories.append(Producer('teddybear', 3, 2, [0,2,2,2]))#left
             self.factories.append(Producer('teddybear', 3, 4, [0,2,0,2]))
@@ -109,9 +112,8 @@ class GameMain():
 
     def LoadSprites(self):
         self.factories = []
-        for i in range(0, 1):
-            producer = Producer('teddybear', 2, 1.5, [0,2,0,0])
-            self.factories.append(producer)
+        producer = Producer('teddybear', 0, 0, [0,2,0,0])
+        self.factories.append(producer)
 
 
 
